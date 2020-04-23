60350d6350852b1159ff594587387b7a99346fb1
Skadi <kartalov.pt@gmail.com>
2020-04-23 19:56:09 +0200

    add cf createActivity

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.createActivity = functions.firestore
+  .document('events/{eventId}')
+  .onCreate(event => {
+    let newEvent = event.data();
+
+    console.log(newEvent);
+
+    const activity = {
+      type: 'newEvent',
+      eventDate: newEvent.date,
+      hostedBy: newEvent.hostedBy,
+      title: newEvent.title,
+      photoURL: newEvent.hostPhotoURL,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      hostUid: newEvent.hostUid,
+      eventId: event.id
+    };
+
+    // There can't be any undefined objects
+    console.log(activity);
+
+    return admin
+      .firestore()
+      .collection('activity')
+      .add(activity)
+      .then(docRef => {
+        return console.log('Activity created, id: ', docRef.id);
+      })
+      .catch(err => {
+        return console.log(err);
+      });
+  });
